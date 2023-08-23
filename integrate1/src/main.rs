86be5ec3b94893f95fa7c74344c05062d10e270a
Mike Moran <mike@houseofmoran.com>
2023-08-23 22:21:05 +0100

    initial `integrate1`, based on `transparent-sprite-text`

---
+++ integrate1/src/main.rs
@@ -0,0 +1,73 @@
+use std::f32::consts::PI;
+
+use bevy::prelude::*;
+use bevy_panorbit_camera::{PanOrbitCameraPlugin, PanOrbitCamera};
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa::default())
+        .add_plugins(DefaultPlugins)
+        .add_plugins(PanOrbitCameraPlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>
+) {
+    // opaque plane, uses `alpha_mode: Opaque` by default
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(12.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        transform: Transform::from_xyz(0.0, -2.0, 0.0),
+        ..default()
+    });
+
+    // load a texture and retrieve its aspect ratio
+    let texture_handle = asset_server.load("branding/bevy_logo_dark_big.png");
+    let aspect = 0.25;
+
+    // create a new quad mesh. this is what we will apply the texture to
+    let quad_width = 8.0;
+    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
+        quad_width,
+        quad_width * aspect,
+    ))));
+
+    // this material renders the texture normally
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(texture_handle.clone()),
+        alpha_mode: AlphaMode::Blend,
+        unlit: true,
+        ..default()
+    });
+
+    // textured quad - normal
+    commands.spawn(PbrBundle {
+        mesh: quad_handle.clone(),
+        material: material_handle,
+        transform: Transform::from_xyz(0.0, 0.0, 1.5)
+            .with_rotation(Quat::from_rotation_x(-PI / 5.0)),
+        ..default()
+    });
+
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn((Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 3.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    }, PanOrbitCamera::default()));
+}
+
