28b9ae51f522718bbc1540aa686f90989fd470d4
Mike Moran <mike@houseofmoran.com>
2023-08-26 17:10:58 +0100

    switch to fractal example temporarily

--- cosmic-text-play/src/main.rs
+++ cosmic-text-play/src/main.rs
@@ -1,54 +1,45 @@
-use cosmic_text::{Attrs, Color, FontSystem, SwashCache, Buffer, Metrics, Shaping};
-use image::Rgb;
+//! An example of generating julia fractals.
+extern crate image;
+extern crate num_complex;
 
 fn main() {
+    let imgx = 800;
+    let imgy = 800;
 
-    // A FontSystem provides access to detected system fonts, create one per application
-    let mut font_system = FontSystem::new();
+    let scalex = 3.0 / imgx as f32;
+    let scaley = 3.0 / imgy as f32;
 
-    // A SwashCache stores rasterized glyphs, create one per application
-    let mut swash_cache = SwashCache::new();
+    // Create a new ImgBuf with width: imgx and height: imgy
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
 
-    // Text metrics indicate the font size and line height of a buffer
-    let metrics = Metrics::new(14.0, 20.0);
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
+    }
 
-    // A Buffer provides shaping and layout for a UTF-8 string, create one per text widget
-    let mut buffer = Buffer::new(&mut font_system, metrics);
+    // A redundant loop to demonstrate reading image data
+    for x in 0..imgx {
+        for y in 0..imgy {
+            let cx = y as f32 * scalex - 1.5;
+            let cy = x as f32 * scaley - 1.5;
 
-    // Borrow buffer together with the font system for more convenient method calls
-    let mut buffer = buffer.borrow_with(&mut font_system);
+            let c = num_complex::Complex::new(-0.4, 0.6);
+            let mut z = num_complex::Complex::new(cx, cy);
 
-    // Set a size for the text buffer, in pixels
-    buffer.set_size(80.0, 25.0);
+            let mut i = 0;
+            while i < 255 && z.norm() <= 2.0 {
+                z = z * z + c;
+                i += 1;
+            }
 
-    // Attributes indicate what font to choose
-    let attrs = Attrs::new();
-
-    // Add some text!
-    buffer.set_text("Hello, Rust! 🦀\n", attrs, Shaping::Advanced);
-
-    // Perform shaping as desired
-    buffer.shape_until_scroll();
-
-    // Inspect the output runs
-    for run in buffer.layout_runs() {
-        for glyph in run.glyphs.iter() {
-            println!("{:#?}", glyph);
+            let pixel = imgbuf.get_pixel_mut(x, y);
+            let data = (*pixel as image::Rgb<u8>).0;
+            *pixel = image::Rgb([data[0], i as u8, data[2]]);
         }
     }
 
-    // Create a default text color
-    let text_color = Color::rgb(0xFF, 0xFF, 0xFF);
-
-    let mut imgbuf: image::ImageBuffer<_, Vec<_>> = image::ImageBuffer::new(80 * 2, 25 * 2);
-
-    // Draw the buffer (for performance, instead use SwashCache directly)
-    buffer.draw(&mut swash_cache, text_color, |x, y, w, h, color| {
-        println!("{x},{y},{w},{h},{color:?}");
-        // Fill in your code here for drawing rectangles
-        let pixel = imgbuf.get_pixel_mut((x + 1) as u32, (y + 1) as u32);
-        *pixel = image::Rgb([0u32, 0u32, 0u32]);
-    });
-
-    // imgbuf.save("image.png").unwrap();
+    // Save the image as “fractal.png”, the format is deduced from the path
+    imgbuf.save("fractal.png").unwrap();
 }
