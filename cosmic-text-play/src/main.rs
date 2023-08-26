a1525445bce141ef905c0c98d669940fcf57c7d3
Mike Moran <mike@houseofmoran.com>
2023-08-26 18:12:13 +0100

    increase size so looks less pixelated

--- cosmic-text-play/src/main.rs
+++ cosmic-text-play/src/main.rs
@@ -1,5 +1,5 @@
+use cosmic_text::fontdb::{Source, Database};
 use cosmic_text::{Attrs, Color, FontSystem, SwashCache, Buffer, Metrics, Shaping};
-use image::Rgb;
 use imageproc::drawing::draw_filled_rect_mut;
 use imageproc::rect::Rect;
 
@@ -8,11 +8,25 @@ fn main() {
     // A FontSystem provides access to detected system fonts, create one per application
     let mut font_system = FontSystem::new();
 
+    let mut db = Database::new();
+    let locale = "en-US";
+
+    // let mut font_system = FontSystem::new_with_fonts(
+    //     vec![Source::File("assets/fonts/FiraSans-Bold.ttf".into())].into_iter()
+    // );
+
+    db.load_fonts_dir("assets/fonts/");
+        
+    // let mut font_system = FontSystem::new_with_locale_and_db(locale.into(), db);
+
+    println!("{:?}", font_system.db());
+
     // A SwashCache stores rasterized glyphs, create one per application
     let mut swash_cache = SwashCache::new();
 
     // Text metrics indicate the font size and line height of a buffer
-    let metrics = Metrics::new(14.0, 20.0);
+    // let metrics = Metrics::new(14.0, 20.0);
+    let metrics = Metrics::new(14.0 * 2.0, 20.0 * 2.0);
 
     // A Buffer provides shaping and layout for a UTF-8 string, create one per text widget
     let mut buffer = Buffer::new(&mut font_system, metrics);
@@ -21,13 +35,18 @@ fn main() {
     let mut buffer = buffer.borrow_with(&mut font_system);
 
     // Set a size for the text buffer, in pixels
-    buffer.set_size(80.0, 25.0);
+    let buf_width = 80.0 * 2.0;
+    let buf_height = 25.0 * 2.0;
+    buffer.set_size(buf_width, buf_height);
 
     // Attributes indicate what font to choose
-    let attrs = Attrs::new();
+    let mut attrs = Attrs::new();
+
+    attrs = attrs.family(cosmic_text::Family::Name("Fira Mono"));
 
     // Add some text!
-    buffer.set_text("Hello, Rust! 🦀\n", attrs, Shaping::Advanced);
+    // buffer.set_text("Hello, Rust! 🦀\n", attrs, Shaping::Advanced);
+    buffer.set_text("Hello, Rust! 🦀\n", attrs, Shaping::Basic);
 
     // Perform shaping as desired
     buffer.shape_until_scroll();
@@ -42,7 +61,7 @@ fn main() {
     // Create a default text color
     let text_color = Color::rgb(0xFF, 0xFF, 0xFF);
 
-    let mut imgbuf: image::ImageBuffer<_, Vec<_>> = image::ImageBuffer::new(80 * 2, 25 * 2);
+    let mut imgbuf: image::ImageBuffer<_, Vec<_>> = image::ImageBuffer::new(buf_width as u32, buf_height as u32);
 
     // Draw the buffer (for performance, instead use SwashCache directly)
     buffer.draw(&mut swash_cache, text_color, |x, y, w, h, color| {
