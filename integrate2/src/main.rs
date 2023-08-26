cd9c33e6d877df97e3da687f2224af4ce1838723
Mike Moran <mike@houseofmoran.com>
2023-08-27 00:48:06 +0100

    show rendered text as texture

--- integrate2/src/main.rs
+++ integrate2/src/main.rs
@@ -1,5 +1,10 @@
 use std::{fs::OpenOptions, error::Error, io::{Write, Cursor}};
 
+use std::f32::consts::PI;
+
+use bevy::prelude::*;
+use bevy_panorbit_camera::{PanOrbitCameraPlugin, PanOrbitCamera};
+
 fn main() -> Result<(), Box<dyn Error>> {
     let message = "hello bevy\nworld!";
     let png_image_bytes = 
@@ -10,12 +15,78 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut file = OpenOptions::new()
         .create(true)
         .write(true)
-        .open("image.png")?;
+        .open("assets/image.png")?;
 
     file.write_all(png_image_bytes.as_slice())?;
 
+    App::new()
+        .insert_resource(Msaa::default())
+        .add_plugins(DefaultPlugins)
+        .add_plugins(PanOrbitCameraPlugin)
+        .add_systems(Startup, setup)
+        .run();
+
     Ok(())
 }
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>
+) {
+    // opaque plane, uses `alpha_mode: Opaque` by default
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(12.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        transform: Transform::from_xyz(0.0, -2.0, 0.0),
+        ..default()
+    });
+
+    // load a texture and retrieve its aspect ratio
+    let texture_handle = asset_server.load("image.png");
+
+    // create a new quad mesh. this is what we will apply the texture to
+    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
+        8.0,
+        2.0,
+    ))));
+
+    // this material renders the texture normally
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(texture_handle.clone()),
+        alpha_mode: AlphaMode::Blend,
+        unlit: true,
+        ..default()
+    });
+
+    // textured quad - normal
+    commands.spawn(PbrBundle {
+        mesh: quad_handle.clone(),
+        material: material_handle,
+        transform: Transform::from_xyz(0.0, 0.0, 1.5)
+            .with_rotation(Quat::from_rotation_x(-PI / 5.0)),
+        ..default()
+    });
+
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn((Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 3.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    }, PanOrbitCamera::default()));
+}
+
+
 
 fn layout_text_as_png_image(
     s: &str, 
